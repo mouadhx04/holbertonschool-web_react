97a48b6da3c89d5ab3e48fe157ff610d9c9a0d63
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-30 20:53:07 +0100

    dashboard main file

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,19 +1,19 @@
-import $ from 'jquery';
-import _ from 'lodash';
-import '../css/main.css';
-
-$('body').append('<div id="logo"></div>');
+import $ from "jquery";
+$.when( $.ready ).then(function() {
+    var count = 0;
+    function updateCounter() {
+        count++;
+        $("#count").html(`${count} clicks on the button`);
+    };
 $('body').append('<p>Holberton Dashboard</p>');
+
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let counter = 0;
+$('body').append('<button>Click here to get started</buttton>');
 
-const updateCounter = () => {
-  counter++;
-  $('#count').html(`${counter} clicks on the button`);
-};
+$('body').append("<p id='count'></p>");
 
 $('button').on('click', _.debounce(updateCounter, 500));
+});
