1beaf47e402dcc1b3089313fd2fa82a5d4d688bd
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-30 18:40:57 +0100

    webpack config file

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,23 +1,15 @@
-// Webpack uses this to work with directories
+
 const path = require('path');
 
-// This is the main configuration object.
-// Here, you write different options and tell Webpack what to do
+
+
+
+
 module.exports = {
-
-  // Path to your entry point. From this file Webpack will begin its work
-  entry: './js/dashboard_main.js',
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on the final bundle. For now, we don't need production's JavaScript 
-  // minifying and other things, so let's set mode to development
-  mode: 'production'
-};
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+      },
+    mode: 'production'
+  };
