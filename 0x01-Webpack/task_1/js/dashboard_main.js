bc58c1948220b5486639f3a70962126eeb45a85f
Mouadh Amemri <mouadh1500@gmail.com>
2022-09-30 20:17:20 +0100

    dashboard main file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,4 @@
-/*import $ from 'jquery';
+import $ from 'jquery';
 import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
@@ -14,20 +14,4 @@ const updateCounter = () => {
   $('#count').html(`${counter} clicks on the button`);
 };
 
-$('button').on('click', _.debounce(updateCounter, 500));*/
-
-
-import $ from "jquery";
-$.when( $.ready ).then(function() {
-    var count = 0;
-    function updateCounter() {
-        count++;
-        $("#count").html(`${count} clicks on the button`);
-    };
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-$('body').append('<button>Click here to get started</buttton>');
-$('body').append("<p id='count'></p>");
 $('button').on('click', _.debounce(updateCounter, 500));
-});
