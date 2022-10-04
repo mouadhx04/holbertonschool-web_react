551f2d6704044f53dd2213d8cc8f1cdb4889dfc2
Mouadh Amemri <mouadh1500@gmail.com>
2022-10-04 18:57:33 +0100

    Commence with class components

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import './App.css';
 import Notifications from '../Notifications/Notifications.js';
 import Header from '../Header/Header.js';
